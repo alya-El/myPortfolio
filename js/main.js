923634e5b06d36922638c6726b1c098133c1c729
Alya <al903325@dal.ca>
2022-06-06 09:18:18 -0300

    added design project

--- js/main.js
+++ js/main.js
@@ -10,6 +10,22 @@ window.onscroll = () => {
     menu.classList.remove('move');
 };
 
+//change header background color when scrolled
+let header = document.querySelector('header');
+
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 0){
+        header.classList.add('header-active');
+    }
+    else if(this.window.scrollY <= 0){
+        header.classList.remove('header-active');
+    }
+});
+
+let page = document.body.id;
+
+switch(page){
+    case 'home':
 //Email JS
 function validate(){
     let name = document.querySelector('.name');
@@ -53,14 +69,48 @@ function success(){
       })
 };
 
-//change header background color when scrolled
-let header = document.querySelector('header');
+//change portfolio boxes according to toggle switch
+var button = document.getElementById('button-container')
+var code = document.getElementById('code')
+var design = document.getElementById('design')
+var buttonTrack = document.getElementById('my-button')
 
-window.addEventListener("scroll", function(){
-    if(this.window.scrollY > 0){
-        header.classList.add('header-active');
+var codeContent = document.querySelectorAll('.code')
+var designContent = document.querySelectorAll('.design')
+
+var buttonState = true;
+
+button.addEventListener('click', function(){
+    if(buttonState){
+        document.getElementById("my-button").style.transform = "translateX(100px)"; 
+        buttonState = false;
+        code.innerText = 'Design'
+        design.innerText = 'Code'
+        design.style.transform = "translateX(-100px)";
+        buttonTrack.style.borderRadius = "0px 20px 20px 0px";
+
+        designContent.forEach(box => {
+            box.style.display = "flex";
+        });
+        codeContent.forEach(box => {
+            box.style.display = "none";
+        });
     }
-    else if(this.window.scrollY <= 0){
-        header.classList.remove('header-active');
-    }
+    else{
+        document.getElementById("my-button").style.transform = "translateX(0px)";
+        buttonState = true;
+        code.innerText = 'Code'
+        design.innerText = 'Design'
+        design.style.transform = "translateX(0px)";
+        buttonTrack.style.borderRadius = "20px 0px 0px 20px";
+
+        designContent.forEach(box => {
+            box.style.display = "none";
+        });
+        codeContent.forEach(box => {
+            box.style.display = "flex";
+        });
+    }    
 });
+break;
+}
